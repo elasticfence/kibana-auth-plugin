750ffbba53142e2018796472ecd940d035ad2a08
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-10-18 17:15:11 +0200

    Add hooks to elasticfence auth

--- server/auth-local-cookie.js
+++ server/auth-local-cookie.js
@@ -1,10 +1,29 @@
-module.exports = function (server) {
-  const users = {};
-  (process.env.LOCAL_AUTH_LOGINS || 'admin:admin').split(',').forEach(function (kv) {
-    var toks = kv.split(':');
-    users[toks[0]] = toks[1];
-  });
+var crypto = require('crypto');
 
+module.exports = function (server) {
+  
+  const encode = function(pwd) {
+    var hash = crypto.createHash('sha256').update(pwd).digest('base64');
+    return hash;
+  };
+  
+  const loginForm = function(reply) {
+       return reply('<!DOCTYPE html><html><head><title>Login Required</title>'
+          + '<link rel="stylesheet" href="/bundles/commons.style.css">'
+          + '<link rel="stylesheet" href="/bundles/kibana.style.css">'
+          + '</head><body>'
+          + '<center><div class="container" style="width: 20%;margin-left: auto;margin-right:auto;margin-top: 10%;">'
+          + '<h1><img width="60%" ng-src="/plugins/kibana/settings/sections/about/barcode.svg" src="/plugins/kibana/settings/sections/about/barcode.svg"></h1>'
+          + (message ? '<h3>' + message + '</h3><br/>' : '')
+          + '<form id="login-form" class="ng-valid ng-dirty ng-valid-parse" method="get" action="/login">'
+          + '<div class="form-group inner-addon left-addon">'
+          + '  <input type="text" style="margin-bottom:8px;font-size: 1.25em;height: auto;" name="username" placeholder="Username" class="form-control ng-valid ng-touched ng-dirty">'
+          + '  <input type="password" style="font-size: 1.25em;height: auto;" name="password" placeholder="Password" class="form-control ng-valid ng-touched ng-dirty">'
+          + '</div><div style="width:200px;margin-left:auto;margin-right:auto;">'
+          + '<input type="submit" value="Login" class="btn btn-default login" style="width: 80%;font-size: 1.5em;">' 
+          + '</div></form></div></center></body></html>'); 
+  }
+  
   const login = function (request, reply) {
 
     if (request.auth.isAuthenticated) {
@@ -22,42 +41,55 @@ module.exports = function (server) {
       username = request.query.username;
       password = request.query.password;
     }
-    var checked = username && users[username] === password;
+    
     if (username || password) {
-      if (!checked) {
-        message = 'Invalid username or password';
-      }
+      var encPass = encode(password);
+      var client = server.plugins.elasticsearch.client;
+      var callWithRequest = server.plugins.elasticsearch.callWithRequest;
+      callWithRequest('search', {
+          index:".http_user_auth",
+          allowNoIndices: false,
+          body: {
+          "size": 1,
+             "query": {
+                "match": {
+                  "username": username
+                 }
+               }
+              }
+       })
+       .then(
+          function (res) {
+            if (res.hits.hits[0] && res.hits.hits[0].password == encPass) {
+                var uuid = 1;
+                const sid = String(++uuid);
+                request.server.app.cache.set(sid, { username: username }, 0, (err) => {
+
+                  if (err) {
+                    reply(err);
+                  }
+
+                  request.auth.session.set({ sid: sid });
+                  return reply.redirect('/');
+                });
+            } else { 
+                checked = false; 
+                message = 'Invalid username or password';
+                loginForm(reply);
+            }
+          },
+          function (error) {
+            var checked = false;
+            message = 'Invalid username or password';
+            loginForm(reply);
+          }
+       );
+      
     } else if (request.method === 'post') {
-      message = 'Missing username or password';
-    }
-    if (!checked) {
-      return reply('<!DOCTYPE html><html><head><title>Login Required</title>'
-          + '<link rel="stylesheet" href="/bundles/commons.style.css">'
-          + '<link rel="stylesheet" href="/bundles/kibana.style.css">'
-          + '</head><body>'
-          + '<center><div class="container" style="width: 20%;margin-left: auto;margin-right:auto;margin-top: 10%;">'
-          + '<h1><img width="60%" ng-src="/plugins/kibana/settings/sections/about/barcode.svg" src="/plugins/kibana/settings/sections/about/barcode.svg"></h1>'
-          + (message ? '<h3>' + message + '</h3><br/>' : '')
-          + '<form id="login-form" class="ng-valid ng-dirty ng-valid-parse" method="get" action="/login">'
-          + '<div class="form-group inner-addon left-addon">'
-          + '  <input type="text" style="margin-bottom:8px;font-size: 1.25em;height: auto;" name="username" placeholder="Username" class="form-control ng-valid ng-touched ng-dirty">'
-          + '  <input type="password" style="font-size: 1.25em;height: auto;" name="password" placeholder="Password" class="form-control ng-valid ng-touched ng-dirty">'
-          + '</div><div style="width:200px;margin-left:auto;margin-right:auto;">'
-          + '<input type="submit" value="Login" class="btn btn-default login" style="width: 80%;font-size: 1.5em;">' 
-          + '</div></form></div></center></body></html>');
+        message = 'Missing username or password';
+        loginForm(reply);
     }
 
-    var uuid = 1;
-    const sid = String(++uuid);
-    request.server.app.cache.set(sid, { username: username }, 0, (err) => {
-
-      if (err) {
-        reply(err);
-      }
-
-      request.auth.session.set({ sid: sid });
-      return reply.redirect('/');
-    });
   };
 
   const logout = function (request, reply) {
